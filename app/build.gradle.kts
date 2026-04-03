f97c237f1a745642e25b02a2dc7805dd521d4fa0
dare-devil-ex <kmsedition@gmail.com>
2026-04-04 03:04:07 +0530

    feat: implement main activity UI with accessibility and configuration toggles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lonemods.overridder"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         buildToolsVersion = "36.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
